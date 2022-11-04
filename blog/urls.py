1e147987723f34fa7e7956a85d9c834889cdd2eb
Arpanish <aparnasapkota4@gmail.com>
2022-11-04 13:36:44 +0545

    Templating

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('blog-list/',views.bindex,name = 'bindex'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,13 @@ from .models import Post
 
 
 def index(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by("-id")[:3]
+    # mathiko code latest post aauxa tin ota
     return render(request,'blog/index.html',{'posts':posts})
     # return render(request,'blog/index.html')
 
     # return HttpResponse("<h1>Hello, world. You're at the polls index.</h1>")
+
+def bindex(request):
+    bposts = Post.objects.all()
+    return render(request,'blog/blog-list.html',{'bcposts':bposts})
